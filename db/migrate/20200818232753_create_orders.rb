42b33097e269fd937658e20986cd37493d41ff6e
barrettanderson <barrettanderson1989@gmail.com>
2020-08-18 16:34:27 -0700

    edited columns for order

--- db/migrate/20200818232753_create_orders.rb
+++ db/migrate/20200818232753_create_orders.rb
@@ -1,6 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.integer :user_id
+      t.integer :food_bank_id
+      t.string :protein
+      t.string :vegetable
+      t.string :fruit
+      t.string :grain
+      t.string :dairy
+      t.string :dessert
 
       t.timestamps
     end
