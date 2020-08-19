d7005d896779ed53cb50c0067ae59b040e07f107
barrettanderson <barrettanderson1989@gmail.com>
2020-08-19 07:38:47 -0700

    changed user column from username to email

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_232922) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
