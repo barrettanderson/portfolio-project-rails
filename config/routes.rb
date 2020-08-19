683ce0316576116dab9cedc59014001c98e61f91
barrettanderson <barrettanderson1989@gmail.com>
2020-08-18 21:35:10 -0700

    custom routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static#home'
+
+  get "/signup", to: "users#new", as: "signup"
+  post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new", as: "login"
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy", as: "logout"
+
   resources :food_banks
   resources :orders
   resources :users
