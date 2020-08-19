d4f9d10032e669e59a57762959263ac42fa52f2b
barrettanderson <barrettanderson1989@gmail.com>
2020-08-18 17:04:32 -0700

    orders controller actions added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,43 @@
 class OrdersController < ApplicationController
+    before_action :set_order, except: [:index, :new, :create]
+
+    def index
+        @orders = Order.all
+
+    end
+
+    def new
+        @order = Order.new
+    end
+
+    def show
+
+    end
+
+    def create
+        
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+        def set_order
+            @order = Order.find_by_id(params[:id])
+        end
+
+        def order_params
+            params.require(:order).permit(:user_id, :food_bank_id, :protein, :vegetable, :fruit, :grain, :dairy, :dessert)
+        end
+
 end
