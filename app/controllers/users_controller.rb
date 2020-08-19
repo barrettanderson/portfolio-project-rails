37075f834ec0738412e883577dd0d9230de791de
barrettanderson <barrettanderson1989@gmail.com>
2020-08-19 14:11:45 -0700

    flash error message for invalid signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to root_path
         else
-            flash[:notice] = "Email already taken"
+            flash.now[:error] = @user.errors.full_messages
             render :new
         end
     end
