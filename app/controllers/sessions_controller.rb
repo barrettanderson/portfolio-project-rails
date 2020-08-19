8ff25b559846a967802b80253c9590e598d5a644
barrettanderson <barrettanderson1989@gmail.com>
2020-08-19 13:53:37 -0700

    adding placeholder values to forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to root_path
         else
+            flash[:notice] = "Invalid email or password"
             redirect_to "/login"
         end
     end
